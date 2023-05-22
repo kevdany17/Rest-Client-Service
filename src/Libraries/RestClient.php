ab239d6ec38dd11020ae3b02c8ec8f397325162c
Kevin Hernandez <kevhernandez07@gmail.com>
2023-05-21 18:33:11 -0600

    feat: Add Unit Test for Methods

--- src/Libraries/RestClient.php
+++ src/Libraries/RestClient.php
@@ -6,120 +6,145 @@ use Exception;
 
 class RestClient
 {
-	//Obtiene los Datos, Apartir de una URL
+	private $curl;
+
+	public function __construct(CurlWrapper $curl = null)
+	{
+		$this->curl = $curl ?? new CurlWrapper();
+	}
+
+	public function setCurl(CurlWrapper $curl)
+	{
+		$this->curl = $curl;
+	}
+	// METHODS
+	// GET Obtiene los Datos, Apartir de una URL
 	public function get($url, $headers = null)
 	{
-		if ($url == "") {
-			throw new Exception("La url esta Vacía.");
-		}
 		$response = null;
 		try {
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			if ($headers != null) {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+			$this->isEmptyUrl($url);
+
+			$this->curl->setOption(CURLOPT_URL, $url);
+			$this->curl->setOption(CURLOPT_RETURNTRANSFER, true);
+			$this->curl->setOption(CURLOPT_FOLLOWLOCATION, true);
+			if (!empty($headers)) {
+				$this->curl->setOption(CURLOPT_HTTPHEADER, $headers);
 			}
-			$curl_response = curl_exec($curl);
+			$curl_response = $this->curl->exec();
 			$response = json_decode($curl_response);
-			curl_close($curl);
 			if ($response === null) {
 				$response = $curl_response;
 			}
 		} catch (\Exception $ex) {
-			throw new \Exception("Get Error: " . $ex->getMessage());
+			throw new \Exception($this->getStringLog('Get', $ex->getMessage()));
+		} finally {
+			$this->curl->close($this->curl);
 		}
 		return $response;
 	}
-	//Post Envía Datos, en una URL
+	// POST Envía Datos, en una URL
 	public function post($url, $data, $headers = null)
 	{
-		if ($url == "") {
-			throw new Exception("La url esta Vacía.");
-		}
 		$response = array();
 		try {
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-			if ($headers != null) {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+			$this->isEmptyUrl($url);
+
+			$this->curl->setOption(CURLOPT_URL, $url);
+			$this->curl->setOption(CURLOPT_POST, true);
+			$this->curl->setOption(CURLOPT_FOLLOWLOCATION, true);
+			if (!empty($headers)) {
+				$this->curl->setOption(CURLOPT_HTTPHEADER, $headers);
+				$this->curl->setOption(CURLOPT_POSTFIELDS, $data);
 			} else {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-				curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+				$this->curl->setOption(CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+				$this->curl->setOption(CURLOPT_POSTFIELDS, json_encode($data));
 			}
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$curl_response = curl_exec($curl);
+			$this->curl->setOption(CURLOPT_RETURNTRANSFER, true);
+			$curl_response = $this->curl->exec($this->curl);
 			$response = json_decode($curl_response);
-			curl_close($curl);
 			if ($response == null) {
 				$response = $curl_response;
 			}
 		} catch (\Exception $ex) {
-			throw new \Exception($ex->getMessage());
+			throw new \Exception($this->getStringLog('Post', $ex->getMessage()));
+		} finally {
+			$this->curl->close($this->curl);
 		}
 		return $response;
 	}
 	//PUT
 	public function put($url, $data, $headers = null)
 	{
-		if ($url == "") {
-			throw new Exception("La url esta Vacía.");
-		}
-		$response = array();
+		$response = null;
 		try {
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
-			if ($headers != null) {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+			$this->isEmptyUrl($url);
+
+			$this->curl->setOption(CURLOPT_URL, $url);
+			$this->curl->setOption(CURLOPT_CUSTOMREQUEST, 'PUT');
+			$this->curl->setOption(CURLOPT_FOLLOWLOCATION, true);
+			if (!empty($headers)) {
+				$this->curl->setOption(CURLOPT_HTTPHEADER, $headers);
+				$this->curl->setOption(CURLOPT_POSTFIELDS, $data);
 			} else {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-				curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+				$this->curl->setOption(CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+				$this->curl->setOption(CURLOPT_POSTFIELDS, json_encode($data));
 			}
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$curl_response = curl_exec($curl);
+			$this->curl->setOption(CURLOPT_RETURNTRANSFER, true);
+			$curl_response = $this->curl->exec($this->curl);
 			$response = json_decode($curl_response);
-			curl_close($curl);
 			if ($response == null) {
 				$response = $curl_response;
 			}
 		} catch (\Exception $ex) {
-			throw new \Exception($ex->getMessage());
+			throw new \Exception($this->getStringLog('Put', $ex->getMessage()));
+		} finally {
+			$this->curl->close($this->curl);
 		}
 		return $response;
 	}
 	//DELETE
 	public function delete($url, $data, $headers = null)
 	{
-		if ($url == "") {
-			throw new Exception("La url esta Vacía.");
-		}
-		$response = array();
+		$response = null;
 		try {
-			$curl = curl_init();
-			curl_setopt($curl, CURLOPT_URL, $url);
-			curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
-			if ($headers != null) {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+			$this->isEmptyUrl($url);
+
+			$this->curl->setOption(CURLOPT_URL, $url);
+			$this->curl->setOption(CURLOPT_CUSTOMREQUEST, 'DELETE');
+			$this->curl->setOption(CURLOPT_FOLLOWLOCATION, true);
+			if (!empty($headers)) {
+				$this->curl->setOption(CURLOPT_HTTPHEADER, $headers);
+				$this->curl->setOption(CURLOPT_POSTFIELDS, $data);
 			} else {
-				curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-				curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+				$this->curl->setOption(CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+				$this->curl->setOption(CURLOPT_POSTFIELDS, json_encode($data));
 			}
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$curl_response = curl_exec($curl);
+			$this->curl->setOption(CURLOPT_RETURNTRANSFER, true);
+			$curl_response = $this->curl->exec($this->curl);
 			$response = json_decode($curl_response);
-			curl_close($curl);
 			if ($response == null) {
 				$response = $curl_response;
 			}
 		} catch (\Exception $ex) {
-			throw new \Exception($ex->getMessage());
+			throw new \Exception($this->getStringLog('Delete', $ex->getMessage()));
+		} finally {
+			$this->curl->close($this->curl);
 		}
 		return $response;
+	}
+
+	// METHODS PRIVATE
+	private function isEmptyUrl($url)
+	{
+		if ($url == '') {
+			throw new Exception('La url esta Vacía.');
+		}
+		return;
+	}
+
+	private function getStringLog($method, $message)
+	{
+		return 'rest-client error [' . $method . ']: ' . $message;
 	}
 }
--- tests/Unit/Libraries/RestClientTest.php
+++ tests/Unit/Libraries/RestClientTest.php
@@ -4,37 +4,212 @@ namespace Tests\Unit\Libraries;
 
 use PHPUnit\Framework\TestCase;
 use Impulzo\RestClientService\Libraries\RestClient;
+use Impulzo\RestClientService\Libraries\CurlWrapper;
 use Exception;
 
 class RestClientTest extends TestCase
 {
 	protected $restClient;
-	protected $mock;
+	protected $curlMock;
+	protected $exampleApi;
+	protected $pokeApi;
 
 	protected function setUp(): void
 	{
 		$this->restClient = new RestClient();
-		$this->mock = $this->getMockBuilder(RestClient::class)
-			->disableOriginalConstructor()
+		$this->curlMock = $this->getMockBuilder(CurlWrapper::class)
 			->getMock();
+		$this->exampleApi = 'https://example.com/api/data';
+		$this->pokeApi = 'https://pokeapi.co/api/v2/pokemon?limit=1&offset=0';
+		$this->body = ['name' => 'Rick', 'last_name' => 'Sánchez'];
 	}
+	// GET
 	public function testGetSuccess()
 	{
 		// Should response success object method GET
-		// $this->mock->expects($this->once())
-        //     ->method('get')
-        //     ->willReturn($this->mock);
-
-		$response = $this->restClient->get("https://pokeapi.co/api/v2/pokemon?limit=1&offset=0");
+		$response = $this->restClient->get($this->pokeApi);
 		$this->assertIsObject($response);
 	}
 	public function testGetErrorUrl()
 	{
 		// Should response error object method GET when url is empty
-		try {
-			$response = $this->restClient->get("");
-		} catch (\Exception $ex) {
-			$this->assertEquals('La url esta Vacía.', $ex->getMessage());
-		}
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage('rest-client error [Get]: La url esta Vacía.');
+
+		$response = $this->restClient->get('');
+	}
+	public function testGetWithHeaders()
+	{
+		// Should response success object method GET with headers
+		$this->curlMock->expects($this->once())->method('exec')->willReturn('{"message":"run on test"}');
+    	$this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+		$headers = array('Content-Type:application/json');
+		$response = $this->restClient->get($this->exampleApi, $headers);
+		$this->assertIsObject($response);
+	}
+	public function testGetWithException()
+	{
+		// Should response error object method GET bad respose
+        $expectedExceptionMessage = 'rest-client error [Get]: Error al obtener los datos.';
+
+        $this->curlMock->expects($this->once())
+			->method('exec')
+			->willThrowException(new \Exception('Error al obtener los datos.'));
+        $this->curlMock->expects($this->once())->method('close');
+
+        $this->restClient->setCurl($this->curlMock);
+
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+		$this->restClient->get($this->exampleApi);
+	}
+
+	// POST
+	public function testPostSuccess()
+	{
+		// Should response success object method POST
+		$this->curlMock->expects($this->once())
+			->method('exec')
+			->willReturn('{"message":"run on test"}');
+        $this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+
+		$response = $this->restClient->post($this->exampleApi, $this->body);
+		$this->assertIsObject($response);
+	}
+	public function testPostErrorUrl()
+	{
+		// Should response error object method POST when url is empty
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage('rest-client error [Post]: La url esta Vacía.');
+
+		$response = $this->restClient->post('', $this->body);
+	}
+	public function testPostWithHeaders()
+	{
+		// Should response success object method POST with headers
+		$this->curlMock->expects($this->once())->method('exec')->willReturn('{"message":"run on test"}');
+    	$this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+		$headers = array('Content-Type:application/json');
+		$response = $this->restClient->post($this->exampleApi, $this->body, $headers);
+		$this->assertIsObject($response);
+	}
+	public function testPostWithException()
+	{
+		// Should response error object method POST bad respose
+        $expectedExceptionMessage = 'rest-client error [Post]: Error al obtener los datos.';
+
+        $this->curlMock->expects($this->once())
+			->method('exec')
+			->willThrowException(new \Exception('Error al obtener los datos.'));
+        $this->curlMock->expects($this->once())->method('close');
+
+        $this->restClient->setCurl($this->curlMock);
+
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+		$this->restClient->post($this->exampleApi, $this->body);
+	}
+
+	// PUT
+	public function testPutSuccess()
+	{
+		// Should response success object method PUT
+		$this->curlMock->expects($this->once())
+			->method('exec')
+			->willReturn('{"message":"run on test"}');
+        $this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+
+		$response = $this->restClient->put($this->exampleApi, $this->body);
+		$this->assertIsObject($response);
+	}
+	public function testPutErrorUrl()
+	{
+		// Should response error object method PUT when url is empty
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage('rest-client error [Put]: La url esta Vacía.');
+
+		$response = $this->restClient->put('', $this->body);
+	}
+	public function testPutWithHeaders()
+	{
+		// Should response success object method PUT with headers
+		$this->curlMock->expects($this->once())->method('exec')->willReturn('{"message":"run on test"}');
+    	$this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+		$headers = array('Content-Type:application/json');
+		$response = $this->restClient->put($this->exampleApi, $this->body, $headers);
+		$this->assertIsObject($response);
+	}
+	public function testPutWithException()
+	{
+		// Should response error object method PUT bad respose
+        $expectedExceptionMessage = 'rest-client error [Put]: Error al obtener los datos.';
+
+        $this->curlMock->expects($this->once())
+			->method('exec')
+			->willThrowException(new \Exception('Error al obtener los datos.'));
+        $this->curlMock->expects($this->once())->method('close');
+
+        $this->restClient->setCurl($this->curlMock);
+
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+		$this->restClient->put($this->exampleApi, $this->body);
+	}
+
+	// DELETE
+	public function testDeleteSuccess()
+	{
+		// Should response success object method DELETE
+		$this->curlMock->expects($this->once())
+			->method('exec')
+			->willReturn('{"message":"run on test"}');
+        $this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+
+		$response = $this->restClient->delete($this->exampleApi, $this->body);
+		$this->assertIsObject($response);
+	}
+	public function testDeleteErrorUrl()
+	{
+		// Should response error object method DELETE when url is empty
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage('rest-client error [Delete]: La url esta Vacía.');
+
+		$response = $this->restClient->delete('', $this->body);
+	}
+	public function testDeleteWithHeaders()
+	{
+		// Should response success object method DELETE with headers
+		$this->curlMock->expects($this->once())->method('exec')->willReturn('{"message":"run on test"}');
+    	$this->curlMock->expects($this->once())->method('close');
+		$this->restClient->setCurl($this->curlMock);
+		$headers = array('Content-Type:application/json');
+		$response = $this->restClient->delete($this->exampleApi, $this->body, $headers);
+		$this->assertIsObject($response);
+	}
+	public function testDeleteWithException()
+	{
+		// Should response error object method DELETE bad respose
+        $expectedExceptionMessage = 'rest-client error [Delete]: Error al obtener los datos.';
+
+        $this->curlMock->expects($this->once())
+			->method('exec')
+			->willThrowException(new \Exception('Error al obtener los datos.'));
+        $this->curlMock->expects($this->once())->method('close');
+
+        $this->restClient->setCurl($this->curlMock);
+
+		$this->expectException(\Exception::class);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+		$this->restClient->delete($this->exampleApi, $this->body);
 	}
 }
